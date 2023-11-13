f69c84b2d794aaf094d4ba400f5af246e0c6d402
Reyjawk <tletempt@gmail.com>
2023-11-13 10:16:11 -0600

    testing only reading first 2 cols

--- main.go
+++ main.go
@@ -2,14 +2,64 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+	"log"
 )
+//Error checking for the file
 func check(e error) {
     if e != nil {
         panic(e)
     }
 }
+//Defining CSV Reader
+type FieldsReader struct {
+	*csv.Reader
+	fields []int
+}
+func NewFieldsReader(r io.Reader, fields ...int) *FieldsReader {
+	fr := &FieldsReader{
+		Reader: csv.NewReader(r),
+		fields: fields,
+	}
+
+	return fr
+}
+
+func (r *FieldsReader) Read() (record []string, err error) {
+	rec, err := r.Reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	record = make([]string, len(r.fields))
+	for i, f := range r.fields {
+		record[i] = rec[f]
+	}
+
+	return record, nil
+}
+
+func (r *FieldsReader) ReadAll() (records [][]string, err error) {
+loop:
+	for {
+		rec, err := r.Read()
+		switch err {
+		case io.EOF:
+			break loop
+		case nil:
+			records = append(records, rec)
+		default:
+			return nil, err
+		}
+	}
+
+	return records, nil
+}
+
 func main() {
 	fmt.Println(welcomeMessage())
 	books, err := os.Open("/Users/toni/GoLang/golang-kata-1/resources/books.csv")
@@ -22,9 +72,15 @@ func main() {
 	}
 	books.Close()
 	for _, line := range scanLines {
-		fmt.Println(line)
+		// We only want first and third columns.
+		r := NewFieldsReader(line, 0, 1)
+		r.Comma = ';'
+		recs, err := r.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(recs)
 	}
-	welcomeMessage()
 }
 
 func welcomeMessage() string {
