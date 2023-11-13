acced4be010fa2040174387ff185e14ef4a44259
Reyjawk <tletempt@gmail.com>
2023-11-13 09:46:48 -0600

    go Kata v1

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
-
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 func main() {
 	fmt.Println(welcomeMessage())
+	books, err := os.Open("/Users/toni/GoLang/golang-kata-1/resources/books.csv")
+    check(err)
+	scanFile := bufio.NewScanner(books)
+	scanFile.Split(bufio.ScanLines)
+	var scanLines []string
+	for scanFile.Scan() {
+		scanLines = append(scanLines, scanFile.Text())
+	}
+	books.Close()
+	for _, line := range scanLines {
+		fmt.Println(line)
+	}
 }
 
 func welcomeMessage() string {
