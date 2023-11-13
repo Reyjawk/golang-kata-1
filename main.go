2aaa010b077ceed74a4487c689ff5d917daf8a13
Reyjawk <tletempt@gmail.com>
2023-11-13 10:41:37 -0600

    testing csv reader

--- main.go
+++ main.go
@@ -14,63 +14,18 @@ func check(e error) {
     if e != nil {
         panic(e)
     }
-}
-//Defining CSV Reader
-type FieldsReader struct {
-	*csv.Reader
-	fields []int
-}
-func NewFieldsReader(r io.Reader, fields ...int) *FieldsReader {
-	fr := &FieldsReader{
-		Reader: csv.NewReader(r),
-		fields: fields,
-	}
-
-	return fr
-}
-
-func (r *FieldsReader) Read() (record []string, err error) {
-	rec, err := r.Reader.Read()
-	if err != nil {
-		return nil, err
-	}
-
-	record = make([]string, len(r.fields))
-	for i, f := range r.fields {
-		record[i] = rec[f]
-	}
-
-	return record, nil
-}
-
-func (r *FieldsReader) ReadAll() (records [][]string, err error) {
-loop:
-	for {
-		rec, err := r.Read()
-		switch err {
-		case io.EOF:
-			break loop
-		case nil:
-			records = append(records, rec)
-		default:
-			return nil, err
-		}
-	}
-
-	return records, nil
-}
-
 func main() {
 	fmt.Println(welcomeMessage())
 	books, err := os.Open("/Users/toni/GoLang/golang-kata-1/resources/books.csv")
     check(err)
+	books.Close()
+	/*
 	scanFile := bufio.NewScanner(books)
 	scanFile.Split(bufio.ScanLines)
 	var scanLines []string
 	for scanFile.Scan() {
 		scanLines = append(scanLines, scanFile.Text())
 	}
-	books.Close()
 	for _, line := range scanLines {
 		// We only want first and third columns.
 		r := NewFieldsReader(line, 0, 1)
@@ -80,6 +35,20 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println(recs)
+	} */
+	//Read the csv file
+	reader := csv.NewReader(books)
+	//Set delimiter
+	reader.Comma = ';'
+	//ReadAll in the file
+	recorders, err := reader.ReadAll()
+	//Checks for errors
+	if err != nil{
+		fmt.Println("Error reading records")
+	}
+	//Loop to iterate through and print each of the string slices
+	for _, eachrecord := range records {
+		fmt.Println(eachrecord)
 	}
 }
 
